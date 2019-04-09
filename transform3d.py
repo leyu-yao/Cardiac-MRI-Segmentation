524d22c4f3908aed244879f4eec2a83c3ad0e486
dynasting <yao.ly@qq.com>
2019-04-09 15:11:14 +0800

    transform created and tested

--- transform3d.py
+++ transform3d.py
@@ -21,15 +21,18 @@ tensor(C,X,Y,Z) => tensor (C,X,Y,Z)
 All transform should have prototype:
 
     X, Y = self.__call__(X, Y)
+    X, Y  should be tensor (C, *, *, *)
 
 usage : tran = transform(para)
         X, Y = tran(X, Y)
 """
-
+import torch
 from torchvision import transforms
-import random
+from random import choice
 
 # ADD Transforms Tomorrow
+
+
 
 class Transpose(object):
 
@@ -37,4 +40,59 @@ class Transpose(object):
         self.dim1=dim1
         self.dim2=dim2
     def __call__(self, X, Y):
-        pass
+        return X.transpose(self.dim1, self.dim2), Y.transpose(self.dim1, self.dim2)
+
+
+class ComposedTransformer(object):
+    '''
+    ComposedTransformer
+    usage :
+        com = ComposedTransformer(t1, t2)
+        # t1 t2 must have __call__(self,X,Y)
+        X,Y=com(X,Y)
+    '''
+    def __init__(self, *args):
+        self.trans = [x for x in args]
+    
+    def __len__(self):
+        return len(self.trans)
+
+    def __getitem__(self, index):
+        return self.trans[index]
+
+    def __call__(self, X, Y):
+        for tran in self.trans:
+            X, Y = tran(X, Y)
+
+        return X, Y
+
+class RandomTransformer(object):
+    '''
+    RandomTransformer
+    usage :
+        com = ComposedTransformer(t1, t2)
+        # t1 t2 must have __call__(self,X,Y)
+        X,Y=com(X,Y)
+    '''
+    def __init__(self, *args):
+        self.trans = [x for x in args]
+    
+    def __len__(self):
+        return len(self.trans)
+
+    def __getitem__(self, index):
+        return self.trans[index]
+
+    def __call__(self, X, Y):
+        tran = choice(self.trans)
+        X, Y = tran(X, Y)
+
+        return X, Y
+
+
+if __name__ == "__main__":
+    X = torch.rand(1,3,4,5)
+    Y = torch.rand(5,3,4,5)
+    
+    tran = Transpose()
+    X, Y = tran(X, Y)
