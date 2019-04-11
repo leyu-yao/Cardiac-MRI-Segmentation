bba3ddebef4f1ec1db7866bca312939e85b69fd4
dynasting <yao.ly@qq.com>
2019-04-11 14:09:13 +0800

    add stride for test

--- test.py
+++ test.py
@@ -63,8 +63,9 @@ class Np_Cutter(object):
     
     generate block_list from array
     '''
-    def __init__(self, block_size):
+    def __init__(self, block_size, stride):
         self.block_size = block_size
+        self.stride = stride
 
     def __call__(self, arr):
         '''
@@ -72,29 +73,29 @@ class Np_Cutter(object):
         return list of (block_arr, (pos_x, pos_y, pos_z))
         '''
         D, H, W = arr.shape
-        d = 1 + int((D-self.block_size[0])/self.block_size[0])
-        h = 1 + int((H-self.block_size[1])/self.block_size[1])
-        w = 1 + int((W-self.block_size[2])/self.block_size[2])
+        d = 1 + int((D-self.block_size[0])/self.stride[0])
+        h = 1 + int((H-self.block_size[1])/self.stride[1])
+        w = 1 + int((W-self.block_size[2])/self.stride[2])
 
         res = []
         
         for dd in range(d+1):
             for hh in range(h+1):
                 for ww in range(w+1):
-                    x1 = dd*self.block_size[0]
-                    x2 = dd*self.block_size[0]+self.block_size[0]
-                    y1 = hh*self.block_size[1]
-                    y2 = hh*self.block_size[1]+self.block_size[1]
-                    z1 = ww*self.block_size[2]
-                    z2 = ww*self.block_size[2]+self.block_size[2]
+                    x1 = dd*self.stride[0]
+                    x2 = dd*self.stride[0]+self.block_size[0]
+                    y1 = hh*self.stride[1]
+                    y2 = hh*self.stride[1]+self.block_size[1]
+                    z1 = ww*self.stride[2]
+                    z2 = ww*self.stride[2]+self.block_size[2]
 
-                    if dd == d:
+                    if x2 > D:
                         x1 = D - self.block_size[0]
                         x2 = D
-                    if hh == h:
+                    if y2 > H:
                         y1 = H - self.block_size[1]
                         y2 = H
-                    if ww == w:
+                    if z2 > W:
                         z1 = W - self.block_size[2]
                         z2 = W
 
@@ -182,7 +183,7 @@ class Test_on_file(object):
     6.init a AUC meter
     '''
 
-    def __init__(self, img_fn, msk_fn, block_size, cvt, net):
+    def __init__(self, img_fn, msk_fn, block_size, cvt, net, stride):
         '''
         block_size tuple
         Read nii file. \n
@@ -195,7 +196,7 @@ class Test_on_file(object):
         self.msk_fn = msk_fn
         (self.img_np, self.mask_np, self.affine) = read_nii_as_np(img_fn, msk_fn, 8)
         #(X,Y,Z) (C,X,Y,Z)
-        self.cutter = Np_Cutter(block_size)
+        self.cutter = Np_Cutter(block_size, stride)
         self.block_list = self.cutter(self.img_np)#[(block_arr, (x, y, z))]
 
         #self.img_tensor = None
@@ -238,7 +239,7 @@ class Test(object):
     on a certain dataset.
     '''
 
-    def __init__(self, workspace, device, ckp, block_size, num_classes):
+    def __init__(self, workspace, device, ckp, block_size, num_classes, stride):
         '''
         construt model and load weights
         make dataset with filename
@@ -250,6 +251,7 @@ class Test(object):
         self.num_classes = num_classes
         self.net = Network_pretrained(ckp, device, num_classes)
         self.cvt = Np_Tensor_Converter(self.device)
+        self.stride = stride
 
     def __call__(self):
         '''
@@ -258,7 +260,7 @@ class Test(object):
         img_msk_fn_list = make_dataset(self.workspace)
 
         for img_fn, msk_fn in img_msk_fn_list:
-            file_test = Test_on_file(img_fn, msk_fn, self.block_size, self.cvt, self.net)
+            file_test = Test_on_file(img_fn, msk_fn, self.block_size, self.cvt, self.net, self.stride)
             score = file_test()
             print(score)
 
@@ -279,12 +281,13 @@ if __name__ == "__main__":
     parse.add_argument("--device", type=str, default="cuda")
     parse.add_argument("--ckp", type=str)
     parse.add_argument("--block_size", type=int, nargs='+', default=(128,128,64))
+    parse.add_argument("--stride", type=int, nargs='+', default=(64,64,32))
     parse.add_argument("--num_classes", type=int, default=5)
 
     args = parse.parse_args()
 
     if args.action == "test":
         test = Test(args.workspace, args.device, args.ckp, 
-                    tuple(args.block_size), args.num_classes)
+                    tuple(args.block_size), args.num_classes, args.stride)
         test()
         
