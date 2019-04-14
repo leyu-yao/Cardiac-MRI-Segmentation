17bfe8929cc5157fadd0531b065e7e9a94404a72
dynasting <yao.ly@qq.com>
2019-04-15 00:32:00 +0800

    use gpu selected, add load weights before training

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 from torch import autograd, optim, nn
 import sys
 import os
+
 import matplotlib.pyplot as plt
 
 # import libs
@@ -52,7 +53,7 @@ def train_model(model, criterion, optimizer, dataload, num_epochs, device, paral
 
     return model
 
-def train(num_classes, batch_size, num_epochs, workspace="./raw", device='cuda', transform=None):
+def train(num_classes, batch_size, num_epochs, workspace="./raw", device='cuda', transform=None, ckp=None):
     '''
     @construct dataloader, criterion, optimizer, construct and train a 3d model
     @input
@@ -64,6 +65,10 @@ def train(num_classes, batch_size, num_epochs, workspace="./raw", device='cuda',
     transform
     '''
     model = models.Unet3d(1, num_classes).to(device)
+    # load weights
+    if ckp is not None:
+        model.load_state_dict(torch.load(ckp, map_location=device))
+
     criterion = losses.DiceLoss(num_of_classes=num_classes, device=device)
     optimizer = optim.Adam(model.parameters())
     ds = dataset.Dataset(workspace, transform=transform, num_classes=num_classes)
@@ -106,7 +111,7 @@ if __name__ == "__main__":
     parse = argparse.ArgumentParser()
     parse.add_argument("action", type=str, help="train or test")
     parse.add_argument("--batch_size", type=int, default=1)
-    parse.add_argument("--ckp", type=str, help="the path of model weight file")
+    parse.add_argument("--ckp", type=str, default=None)
     parse.add_argument("--num_epochs", type=int, default=5)
     parse.add_argument("--device", type=str, default="cuda")
     parse.add_argument("--para", type=bool, default=False)
@@ -118,14 +123,14 @@ if __name__ == "__main__":
 
 
 
-    #os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_index
-
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_index
+    #os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
     if args.action == "train":
         #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         device = torch.device(args.device)
         tran = transforms.DownSample((args.resolution))
-        train(args.num_classes, args.batch_size, args.num_epochs, args.workspace, device=device, transform=tran)
+        train(args.num_classes, args.batch_size, args.num_epochs, args.workspace, device=device, transform=tran, ckp=args.ckp)
 
     elif args.action == "test":
         device = torch.device(args.device)
