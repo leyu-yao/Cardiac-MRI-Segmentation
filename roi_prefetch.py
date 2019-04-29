8ed422fbc001745502a996c295ef9390c0b725cf
dynasting <yao.ly@qq.com>
2019-04-29 22:24:38 +0800

    START ROI

--- roi_prefetch.py
+++ roi_prefetch.py
@@ -29,27 +29,3 @@ def roi_detect_np(y):
 
     return (slice(rmin, rmax), slice(cmin, cmax), slice(zmin, zmax))
 
-def roi_train_prerare(dst_dir, src_dir, num_classes=8):
-    '''
-    read nii files as np
-    slice roi
-    get one-hot 
-    output numpy arr in file system
-    '''
-    img_msk_list = util.make_dataset(src_dir)
-
-    idx = 0
-
-
-    for x_path, y_path in img_msk_list: 
-
-        img_nib = nib.load(x_path)
-        mask_nib = nib.load(y_path)
-
-        # np.array D,H,W
-        x = img_nib.get_fdata().astype(np.float32)
-        y = mask_nib.get_fdata().astype(np.float32)
-
-        roi_slice = roi_detect_np(y)
-
-        
---
+++ roi_show_range.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 21:38:40 2019
+
+@author: Dynasting
+"""
+
